b9f003cacd653441ada8918a5cec35fb7b113ebf
kaerimashou <58917613+kaerimashou@users.noreply.github.com>
2020-10-28 13:57:00 +0300

    Calculator
    Имеется калькулятор, который выполняет три операции:
    
    Прибавить к числу X единицу.
     Умножить число X на 2.
    Умножить число X на 3.
    Определите, какое наименьшее число операций необходимо для того, чтобы получить из числа 1 заданное число N.
    
    Входные данные
    Программа получает на вход одно число, не превосходящее 106.
    
    Выходные данные
    Требуется вывести одно число: наименьшее количество искомых операций.

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<cstring>
+int N;
+struct op {
+    unsigned count_op;
+    unsigned code_op;
+    unsigned operand;
+}x[1000001];
+void print(int operand) {
+    if (operand != 1) {
+        print(x[operand].operand);
+        std::cout << x[operand].code_op;
+    }
+}
+int main()
+{
+    std::cin >> N;
+    memset(x, 0xFF, (N + 1) * sizeof(op));
+    x[1] = { 0 };
+    for (int i = 1; i < N; ++i)
+    {
+        if (x[i + 1].count_op > x[i].count_op + 1)
+        {
+            x[i + 1].count_op = x[i].count_op + 1;
+            x[i + 1].code_op = 1;
+            x[i + 1].operand = i;
+        }
+        if (i * 2 <= N && x[i * 2].count_op > x[i].count_op + 1)
+        {
+            x[i * 2].count_op = x[i].count_op + 1;
+            x[i * 2].code_op = 2;
+            x[i * 2].operand = i;
+        }
+        if (i * 3 <= N && x[i * 3].count_op > x[i].count_op + 1)
+        {
+            x[i * 3].count_op = x[i].count_op + 1;
+            x[i * 3].code_op = 3;
+            x[i * 3].operand = i;
+        }
+    }
+    print(N);
+}
---
+++ Calculator/Source1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+const int size = 10;
+int main() {
+	int n;
+	std::cin >> n;
+	int a[size][size];
+	for (int i = 0; i < n; i++) {
+		a[i][0] = 1;
+		a[i][i] = 1;
+		for (int j = 1; j < i; j++) {
+			a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			std::cout << a[i][j];
+		}
+	}
+}
