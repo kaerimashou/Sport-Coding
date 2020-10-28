2c5f723668d74b2639b4a887ca86c74d3c1a9f2a
kaerimashou <58917613+kaerimashou@users.noreply.github.com>
2020-10-28 14:07:50 +0300

    MatrixPath
    Вывести количество путей из левого верхнего угла матрицы NxM в правый нижний
    
    Входные данные:
    n и m - размеры матрицы
    
    Выходные данные:
    Количество путей

---
+++ MatrixPath/MatrixPath.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+static int n;
+static int m;
+int find(int n,int m) {
+	if (n == 0 || m == 0) {
+		return 1;
+	}
+	else {
+		return find(n, m - 1) + find(n - 1, m);
+	}
+}
+int main() {
+	std::cin >> n >> m;
+	std::cout << find(n-1, m-1);
+}
