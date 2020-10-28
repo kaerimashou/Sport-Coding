7b792fab3e11501b176f6077b755b97086f9d9c3
kaerimashou <58917613+kaerimashou@users.noreply.github.com>
2020-10-28 14:27:59 +0300

    Попытка к бегству
    Узник пытается бежать из замка, который состоит из N×M квадратных комнат, расположенных в виде прямоугольника NxM. Между любыми двумя соседними комнатами есть дверь, однако некоторые комнаты закрыты и попасть в них нельзя. В начале узник находится в левой верхней комнате и для спасения ему надо попасть в противоположную правую нижнюю комнату. Времени у него немного, всего он может побывать не более, чем в N+M-1 комнате на своем пути, то есть перемещаться он должен только вправо или вниз. Определите количество маршрутов, которые ведут к выходу.
    
    Входные данные
    Первая строчка входных данных содержит натуральные числа N и M, не превосходящих 1000. Далее идет план замка в виде N строчек из M чисел в каждой. Одно число соответствует одной комнате: 1 означает, что в комнату можно попасть, 0 – что комната закрыта.
    
    Выходные данные
    Программа должна напечатать количество маршрутов, ведущих узника к выходу и проходящих через M+N-1 комнату, или слово Impossible, если таких маршрутов не существует.
    
    Входные данные подобраны таким образом, что искомое число маршрутов не превосходит 2.000.000.000.

---
+++ Runaway/Runaway.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+const int size = 1000;
+static long int a[size][size];
+static long int b[size][size];
+static int n;
+static int m;
+void initalize(int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			b[i][j] = -1;
+		}
+	}
+	b[0][0] = 1;
+}
+bool av(int i, int j) {
+	return(i >= 0) && (j >= 0) &&(i<n) && (j<m) && (a[i][j] != 0);
+}
+int move(int i, int j) {
+	if (av(i, j)) {
+		
+		if (b[i][j] == -1) {
+			b[i][j] = move(i - 1, j) + move(i, j - 1);
+		}
+	}
+	else {
+		return 0;
+	}
+	return b[i][j];
+}
+int main() {
+	std::cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			std::cin >> a[i][j];
+		}
+	}
+	initalize(n, m);
+	int c = move(n - 1, m - 1);
+	if (c == 0) {
+		std::cout << "Impossible";
+	}
+	else {
+		std::cout << c;
+	}
+}
