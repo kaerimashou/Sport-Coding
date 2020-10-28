d982954e956c59aa2058150afb73de2c605a1bac
kaerimashou <58917613+kaerimashou@users.noreply.github.com>
2020-10-28 13:52:08 +0300

    Cheapest Way
    В каждой клетке прямоугольной таблицы N×M записано некоторое число. Изначально игрок находится в левой верхней клетке. За один ход ему разрешается перемещаться в соседнюю клетку либо вправо, либо вниз (влево и вверх перемещаться запрещено). При проходе через клетку с игрока берут столько килограммов еды, какое число записано в этой клетке (еду берут также за первую и последнюю клетки его пути).
    
    Требуется найти минимальный вес еды в килограммах, отдав которую игрок может попасть в правый нижний угол.
    
    Входные данные
    Вводятся два числа N и M — размеры таблицы (1≤N≤20, 1≤M≤20). Затем идет N строк по M чисел в каждой — размеры штрафов в килограммах за прохождение через соответствующие клетки (числа от 0 до 100).
    
    Выходные данные
    Выведите минимальный вес еды в килограммах, отдав которую можно попасть в правый нижний угол.

---
+++ CheapestWay/CheapestWay.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+
+const int sz = 20;
+static int a[sz][sz];
+static int b[sz][sz];
+static int n;
+static int m;
+
+int main() {
+	std::cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			std::cin >> a[i][j];
+		}
+	}
+	b[0][0] = a[0][0];
+	for (int i = 1; i < n; i++) {
+		b[i][0] = b[i - 1][0] + a[i][0];
+	}
+	for (int i = 1; i < m; i++) {
+		b[0][i] = b[0][i-1] + a[0][i];
+	}
+	for (int i = 1; i < n; i++) {
+		
+		for (int j = 1; j < m; j++) {
+			b[i][j] = std::min(b[i][j - 1], b[i - 1][j]) + a[i][j];
+		}
+	}
+	std::cout << b[n-1][m-1];
+}
