d37f07a757009c2c4c9048f99b3265b15b89f2e1
kaerimashou <58917613+kaerimashou@users.noreply.github.com>
2020-10-28 13:54:18 +0300

    Shashki
    На шахматной доске (8x8) стоит одна белая шашка. Сколькими способами она может пройти в дамки?
    
    (Белая шашка ходит по диагонали. на одну клетку вверх-вправо или вверх-влево. Шашка проходит в дамки, если попадает на верхнюю горизонталь.)
    
    Входные данные
    Вводятся два числа от 1 до 8: номер номер столбца (считая слева) и строки (считая снизу), где изначально стоит шашка.
    
    Выходные данные
    Вывести одно число - количество путей в дамки.

---
+++ Shashki/Shashki.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+const int size = 50;
+static int n;
+static int m;
+static int a[size][size];
+
+void initialize(int n,int m,int a[size][size]) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			a[i][j] = -1;
+		}
+	}
+	a[0][0] = 1;
+}
+
+bool good(int i, int j, int n, int m)
+{
+	return (i >= 0) && (j >= 0) && (i < n) && (j < m);
+}
+
+int solve(int i, int j,int n,int m,int a[size][size])
+{
+	if (good(i, j, n, m))
+	{
+		if (a[i][j] == -1)
+			a[i][j] = solve(i - 2, j - 1,n,m,a) + solve(i - 2, j + 1,n,m,a) + solve(i - 1, j - 2,n,m,a) + solve(i + 1, j - 2,n,m,a);
+	}
+	else
+		return 0;
+	return a[i][j];
+}
+	
+int main() {
+	std::cin >> m >> n;
+	initialize(n, m, a);
+	std::cout << solve(n-1,m-1,n,m,a);
+}
