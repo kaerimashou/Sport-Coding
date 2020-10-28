ae068c54704644306176f31a8760fe760c8dc959
kaerimashou <58917613+kaerimashou@users.noreply.github.com>
2020-10-28 13:58:40 +0300

    Calculator2
    Имеется калькулятор, который выполняет три операции:
    
    Прибавить к числу X единицу.
     Умножить число X на 2.
    Умножить число X на 3.
    Определите, какое наименьшее число операций необходимо для того, чтобы получить из числа 1 заданное число N.
    
    Входные данные
    Программа получает на вход одно число, не превосходящее 106.
    
    Выходные данные
    Требуется вывести одно число: наименьшее количество искомых операций.

---
+++ Calculator2/Calculator2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+static int a;
+static int n;
+int amount(int n) {
+	if (n == 1) {
+		return 0;
+	}
+	else {
+		if (n % 3 == 0) {
+			return amount(n / 3) + 1;
+		}
+		else {
+			if (n % 2 == 0 && amount(n - 1) > amount(n / 2)) {
+				return amount(n / 2) + 1;
+			}
+			else {
+				return amount(n - 1) + 1;
+			}
+		}
+	}
+}
+int main() {
+	cin >> n;
+	cout << endl << amount(n);
+}
